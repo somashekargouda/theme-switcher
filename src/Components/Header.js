e6dc4c23a4c17a7680d17098609c2339b74708bf
somashekar gouda <somashekar.patil09@gmail.com>
2021-03-14 02:01:59 +0530

    First commit

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React, { useContext } from "react";
+import ThemeToggler from "./ThemeToggler";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Theme toggler</h1>
+      <ThemeToggler />
+    </header>
+  );
+};
+
+export default Header;
